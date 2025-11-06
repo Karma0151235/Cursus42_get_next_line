b34fa77426689db7e7eabb5c2e6771a9d72d7498
Stephen Tee <stephentee88@gmail.com>
2025-11-06 21:38:41 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include "get_next_line.h"
+
+int  main(void)
+{
+    char  *line;
+    int    fd = open("test.txt", O_RDONLY);
+
+    line = get_next_line(fd);
+    printf("Line 1: %s", line);
+    free(line);
+
+    line = get_next_line(fd);
+    printf("Line 2: %s", line);
+    free(line);
+
+    line = get_next_line(fd);
+    printf("Line 3: %s", line);
+    free(line);
+
+    line = get_next_line(fd);
+    printf("Line 4: %s", line);
+    free(line);
+
+    line = get_next_line(fd);
+    printf("Line 5: %s", line);
+    free(line);
+
+    printf('\n');
+
+    close(fd);
+    return (0);
+}
